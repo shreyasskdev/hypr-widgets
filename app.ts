8710b30fa6c5e9224304c9065156a337fe04bafa
Shreyas sk <shreyassk.dev@gmail.com>
2025-05-16 23:04:16 +0530

    feat: Realtime css switch added, Hardcoded css path removed

--- app.ts
+++ app.ts
@@ -4,10 +4,16 @@ import Bar from "./widget/Bar/Bar";
 import DesktopClock from "./widget/DesktopClock";
 import ChargingAlert from "./widget/ChargingAlert";
 import NotificationPopups from "./widget/Notification/NotificationPopup";
+import { monitorFile, readFile, writeFile } from "astal";
+import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 
 App.start({
   css: style,
   main() {
+    const colors = readFile("./styles/colors.css");
+    App.apply_css(colors);
+
     App.get_monitors().forEach((gdkmonitor) => {
       Bar(gdkmonitor);
       DesktopClock(gdkmonitor);
@@ -16,3 +22,12 @@ App.start({
     });
   },
 });
+
+monitorFile(
+  `${GLib.get_home_dir()}/.cache/wal/colors.css`,
+  (file: string, event: Gio.FileMonitorEvent) => {
+    const content = readFile(file);
+    writeFile("./styles/colors.css", content);
+    App.apply_css(content);
+  },
+);
